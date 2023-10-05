a49b8626f5125c960d50036a8757d9dbac909bbf
Ignacio Mosconi <134315575+ignamosconi@users.noreply.github.com>
2023-10-05 16:31:49 -0300

    v1.04 - Personaje test

--- agujerosAtómicosGreenfoot/MyWorld.java
+++ agujerosAtómicosGreenfoot/MyWorld.java
@@ -1,21 +1,17 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
-/**
- * Write a description of class MyWorld here.
- * 
- * @author (your name) 
- * @version (a version number or a date)
- */
 public class MyWorld extends World
 {
 
     /**
-     * Constructor for objects of class MyWorld.
-     * 
+     * Constructor para el tablero
      */
     public MyWorld()
     {    
-        // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
-        super(600, 400, 1); 
+        // Creamos un mundo de 700 x 700 pixeles, con celdas de 1x1 pixel.
+        super(700, 700, 1); 
+
+        //Establecemos el fondo a la imagen que colocamos en 
+        setBackground("fondoTablero.png");
     }
 }
